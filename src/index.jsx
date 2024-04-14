c8694769c106af49a68f5574ecf8a6570d3c8dc0
Abner Antonio Chamorro <achamorro@pmac-achamorro.local>
2024-04-14 17:25:58 -0500

    fix eslint in flie index

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-const root = ReactDOM.createRoot(document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById('app'))
 
 
 root.render(
