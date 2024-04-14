4dc21a878f7a8946247bb98aa53fee1ecec68d14
Abner Antonio Chamorro <achamorro@pmac-achamorro.local>
2024-04-14 17:23:30 -0500

    Fix slint

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
